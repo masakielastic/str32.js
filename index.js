8045f2dfc45fbc3105cde1912a0e022fa9db0677
Masaki Kagaya <masakielastic@gmail.com>
2015-09-04 06:14:59 +0900

    fixed return statement

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.countChar32 = function(str) {
-  return return str.length - (str.match(/[\uD800-\uDBFF][\uDC00-\uDFFFF]/g)||[]).length;  
+  return str.length - (str.match(/[\uD800-\uDBFF][\uDC00-\uDFFFF]/g)||[]).length;  
 };
