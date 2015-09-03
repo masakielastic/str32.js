038c41e90ef1b02f9587e73493585e773c1f2afc
Masaki Kagaya <masakielastic@gmail.com>
2015-09-04 07:08:05 +0900

    added length and charAt method

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-exports.countChar32 = function(str) {
+exports.length = function(str) {
   return str.length - (str.match(/[\uD800-\uDBFF][\uDC00-\uDFFFF]/g)||[]).length;  
 };
+
+exports.charAt = function(str, offset) {
+  var pos = 0;
+  var length = str.length;
+  var cp = 0;
+  var size = 0;
+  var index = 0;
+
+  while (pos < length) {
+    cp = str.codePointAt(pos);
+    size = cp < 0x10000 ? 1 : 2;
+
+    if (index === offset) {
+      return str.substr(pos, size);
+    }
+
+    pos += size;
+    ++index;
+  }
+
+  return '';
+}
