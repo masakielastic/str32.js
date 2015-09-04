10ab54a9cd6d0a9e48a2f6b7d2ce533ca8d45dd5
Masaki Kagaya <masakielastic@gmail.com>
2015-09-04 14:18:44 +0900

    added test case for substr

--- test/test.js
+++ test/test.js
@@ -4,11 +4,15 @@ var utils = require('../');
 describe('StrUtils', function() {
   var str = '\uD842\uDFB7野家';
 
-  it('文字列の長さは3になる。', function() {
+  it('文字列の長さは3である。', function() {
     assert(3, utils.length(str));
   });
 
-  it('サロゲートペアを考慮して1文字取得する', function() {
-    assert('\uD842\uDFB7' === str.charAt(0));
+  it('サロゲートペアを考慮して1文字取得する。', function() {
+    assert('\uD842\uDFB7' === utils.charAt(str, 0));
+  });
+
+  it('サロゲートペアを考慮して2文字取得する。', function() {
+    assert('\uD842\uDFB7野' === utils.substr(str, 0, 2));
   });
 });
